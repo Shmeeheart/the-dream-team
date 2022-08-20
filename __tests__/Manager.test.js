2f640e0c24c268897c3ab582786ecf57bb3eb03d
Jennifer <goodwije@gmail.com>
2022-08-20 10:37:21 -0600

    added Manager tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,17 +1,31 @@
-const Employee = require('../lib/Employee');
+const Manager = require('../lib/Employee');
 
-describe('Employee', () => {
+describe('Manager', () => {
   describe('object', () => {
     it('should be an object', () => {
-      let tempEmp = new Employee();
-      expect(typeof tempEmp).toEqual('object');
+      let tempMan = new Manager();
+      expect(typeof tempMan).toEqual('object');
     });
   });
   describe('name', () => {
     it('should describe the name', () => {
       let name = 'somename';
-      let tempEmp = new Employee(name);
-      expect(tempEmp.name).toEqual(name);
+      let tempMan = new Manager(name);
+      expect(tempMan.name).toEqual(name);
+    });
+  });
+  describe('email', () => {
+    it('should display the email', () => {
+      let email = 'someemail';
+      let tempEmail = new Email(email);
+      expect(tempEmail.email).toEqual(email);
+    });
+  });
+  describe('officeNumber', () => {
+    it('should describe the office number', () => {
+      let officeNumber = 'someofficeNumber';
+      let tempOff = new officeNumber(officeNumber);
+      expect(tempOff.officeNumber).toEqual(officeNumber);
     });
   });
 });
